b1a8ae53eb2df56756347d5d51aab05422206cb4
waydanshcodes <waydansh@gmail.com>
2025-08-14 02:22:52 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Categories/>
       <BestSeller />
       <BottomBanner />
+      <NewsLetter />
     </div>
   )
 }
