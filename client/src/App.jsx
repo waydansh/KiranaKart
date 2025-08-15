94f1ef2f6f66c9c7b7157efbd814d3d516bc9c70
waydanshcodes <waydansh@gmail.com>
2025-08-16 01:02:42 +0530

    add-address page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import AllProducts from './pages/AllProducts';
 import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
+import AddAddress from './pages/AddAddress';
 
 const App = () => {
 
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/add-address" element={<AddAddress />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
---
+++ client/src/pages/AddAddress.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
+
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+    <input
+        className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
+        type={type}
+        placeholder={placeholder}
+        onChange={handleChange}
+        name={name}
+        value={address[name]}
+        required
+    />
+)
+
+const AddAddress = () => {
+
+    const { user, navigate } = useAppContext();
+
+    const [address, setAddress] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        street: '',
+        city: '',
+        state: '',
+        zipcode: '',
+        country: '',
+        phone: ''
+    })
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setAddress((prevAddress) => ({
+            ...prevAddress,
+            [name]: value
+        }))
+    }
+
+    return (
+
+        <div className='mt-16 pb-16'>
+            <p className='text-2x1 md:text-3x1 text-gray-500' >Add Shipping
+                <span className='font-semibold text-primary' >Address</span>
+            </p>
+            <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
+                <div className='flex-1 max-w-md'>
+                    <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
+                        <div>
+                            <InputField handleChange={handleChange} address={address} name='firstName' type='text' placeholder='First Name'></InputField>
+                            <InputField handleChange={handleChange} address={address} name='lastName' type='text' placeholder='Last Name'></InputField>
+                        </div>
+                        <InputField handleChange={handleChange} address={address} name="email" type="email" placeholder="Email Address" />
+                        <InputField handleChange={handleChange} address={address} name="street" type="text" placeholder="Street" />
+                        <div className="grid grid-cols-2 gap-4">
+                            <InputField handleChange={handleChange} address={address} name="city" type="text" placeholder="City" />
+                            <InputField handleChange={handleChange} address={address} name="state" type="text" placeholder="State" />
+                        </div>
+                        <div className="grid grid-cols-2 gap-4">
+                            <InputField handleChange={handleChange} address={address} name="country" type="text" placeholder="Country" />
+                        </div>
+                        <button className="w-full mt-6 bg-primary text-white py-3 hover:bg-primary-dull transition cursor-pointer uppercase">Save Address</button>
+                    </form>
+                </div>
+                <img src={assets.add_address_iamge} alt="Add Address" />
+            </div>
+        </div >
+    )
+}
+
+export default AddAddress
