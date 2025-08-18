090ef149fa8a9e4dab346921d7f507c1756da304
waydanshcodes <waydansh@gmail.com>
2025-08-19 02:53:04 +0530

    product-list page

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -30,7 +30,7 @@ const SellerLayout = () => {
                 </div>
             </div>
             <div className="flex ">
-                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
                     {sidebarLinks.map((item) => (
                         <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
                             className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
