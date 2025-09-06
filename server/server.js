71022a170c4246639f1af23d829988a5cb3e3bf2
waydanshcodes <waydansh@gmail.com>
2025-09-06 20:26:21 +0530

    fe url

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB();
 await connectCloudinary();
 
 // allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://kirana-kart.vercel.app']
 
 // Middleware to parse JSON bodies
 app.use(express.json());
